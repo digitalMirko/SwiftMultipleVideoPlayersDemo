5ac6839231cb9fe01d4173124d06a1ee0796d671
Mirko Cukich <mirkocukich@gmail.com>
2019-12-11 00:09:43 -0600

    Local Player Demo Part - Program Complete

--- Multiple Video Players/ViewController.swift
+++ Multiple Video Players/ViewController.swift
@@ -7,19 +7,33 @@
 //
 
 import UIKit
+import AVFoundation
+import AVKit
 
 class ViewController: UIViewController {
+    
+    // Local Player Demo
+    var localPlayerController = AVPlayerViewController()
+    var localPlayer: AVPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Local Player Demo
+        let localVideoName = Bundle.main.path(forResource: "SampleVideo_1280_10MG", ofType: "mp4")
         
+        localPlayer = AVPlayer(url: URL(fileURLWithPath: localVideoName!))
+        localPlayerController.player = localPlayer
         
     }
 
-    
+    // Local Player Demo
     @IBAction func playLocalVideo(_ sender: Any) {
         
+        self.present(self.localPlayerController, animated: true, completion: {
+            
+            self.localPlayerController.player?.play()
+        })
     }
     
 
